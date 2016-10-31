e33e053d0d5b1ec6b48ebae9147990bb4b8eadef
codetheorist <codetheorist@gmail.com>
2016-10-31 10:45:14 +0000

    Add basic plugin settings & empty function expression

--- src/js/shapeshifter.js
+++ src/js/shapeshifter.js
@@ -2,6 +2,16 @@
 
   $.fn.bgAnim = function(options) {
 
+    var settings = $.extend( {
+      'selector'      : '.shapes',
+      'resizeDelay'   : 200,
+      'shapeType'     : 'triangle'
+    }, options);
+
+    var shapeshifter = function(element, i) {
+
+    };
+
   };
 }( jQuery, window, document, undefined ));
 
